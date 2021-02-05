e0c239aecdd16ed9192288f5eb31616b0d388e02
pooria1 <poori25.k@gmail.com>
2021-02-05 13:38:06 +0330

    getSyscallCountTest gets argument as syscall ID

--- getSyscallCounterTest.c
+++ getSyscallCounterTest.c
@@ -2,14 +2,22 @@
 #include "stat.h"
 #include "user.h"
 
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc < 2) {
+        printf(1, "Pass an argument as system call ID\n");
+        exit();
+        return 0;
+    }
+
+    int syscallID = atoi(argv[1]);
+
     getpid();
     getpid();
     getpid();
     getpid();
 
-    printf(1, "we called getpid syscall %d time(s)\n", getSyscallCounter(11));
+    printf(1, "syscall ID=%d, %d time(s) called!\n", syscallID, getSyscallCounter(syscallID));
 
     exit();
     return 0;
